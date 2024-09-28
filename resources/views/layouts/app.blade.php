84010f05b531c656c6a092138d97cf5854fcf3a9
shofiudinmuh <shofiudinmuh@gmail.com>
2024-09-28 12:10:45 +0700

    add templating layout and assets for admin

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,68 +1,122 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html>
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('assets/build/assets/img/apple-icon.png') }}" />
+    <link rel="icon" type="image/png" href="{{ asset('assets/build/assets/img/favicon.png') }}" />
+    <title>Toko Onlineku | Dashboard</title>
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
-    <link rel="stylesheet" href="{{ asset('assets/css/tailwind.css') }}">
-    <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-
-    <!-- Styles -->
-    @livewireStyles
+    @include('admin.partials.link')
 </head>
 
-<body class="font-sans antialiased"
-    x-data="{ page: 'analytics', 'loaded': true, 'darkMode': true, 'stickyMenu': false, 'sidebarToggle': false, 'scrollTop': false }"
-    x-init="
-         darkMode = JSON.parse(localStorage.getItem('darkMode'));
-         $watch('darkMode', value => localStorage.setItem('darkMode', JSON.stringify(value)))"
-    :class="{'dark text-bodydark bg-boxdark-2': darkMode === true}">
-    <x-banner />
+<body class="m-0 font-sans text-base antialiased font-normal leading-default bg-gray-50 text-slate-500">
 
-    @include('admin.partials.preloader')
-    <div class="flex h-screen overflow-hidden">
-        {{-- @livewire('navigation-menu') --}}
+    @include('admin.partials.sidebar')
 
-        {{--
-        <!-- Page Heading -->
-        @if (isset($header))
-        <header class="bg-white shadow">
-            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                {{ $header }}
+    <main class="ease-soft-in-out xl:ml-68.5 relative h-full max-h-screen rounded-xl transition-all duration-200">
+        <!-- Navbar -->
+
+        @include('admin.partials.navbar')
+
+        <!-- end Navbar -->
+
+        <!-- cards -->
+        <div class="w-full px-6 py-6 mx-auto">
+            {{ $slot }}
+
+            {{-- footer --}}
+            @include('admin.partials.footer')
+            {{-- end of footer --}}
+        </div>
+        <!-- end cards -->
+    </main>
+    <div fixed-plugin>
+        <a fixed-plugin-button
+            class="bottom-7.5 right-7.5 text-xl z-990 shadow-soft-lg rounded-circle fixed cursor-pointer bg-white px-4 py-2 text-slate-700">
+            <i class="py-2 pointer-events-none fa fa-cog"> </i>
+        </a>
+        <!-- -right-90 in loc de 0-->
+        <div fixed-plugin-card
+            class="z-sticky shadow-soft-3xl w-90 ease-soft -right-90 fixed top-0 left-auto flex h-full min-w-0 flex-col break-words rounded-none border-0 bg-white bg-clip-border px-2.5 duration-200">
+            <div class="px-6 pt-4 pb-0 mb-0 bg-white border-b-0 rounded-t-2xl">
+                <div class="float-left">
+                    <h5 class="mt-4 mb-0">Soft UI Configurator</h5>
+                    <p>See our dashboard options.</p>
+                </div>
+                <div class="float-right mt-6">
+                    <button fixed-plugin-close-button
+                        class="inline-block p-0 mb-4 text-xs font-bold text-center uppercase align-middle transition-all bg-transparent border-0 rounded-lg shadow-none cursor-pointer hover:scale-102 leading-pro ease-soft-in tracking-tight-soft bg-150 bg-x-25 active:opacity-85 text-slate-700">
+                        <i class="fa fa-close"></i>
+                    </button>
+                </div>
+                <!-- End Toggle Button -->
             </div>
-        </header>
-        @endif --}}
-
-        @include('admin.partials.sidebar')
-
-        <div class="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
-
-            @include('admin.partials.header')
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-
+            <hr class="h-px mx-0 my-1 bg-transparent bg-gradient-to-r from-transparent via-black/40 to-transparent" />
+            <div class="flex-auto p-6 pt-0 sm:pt-4">
+                <!-- Sidebar Backgrounds -->
+                <div>
+                    <h6 class="mb-0">Sidebar Colors</h6>
+                </div>
+                <a href="javascript:void(0)">
+                    <div class="my-2 text-left" sidenav-colors>
+                        <span
+                            class="text-xs rounded-circle h-5.75 mr-1.25 w-5.75 ease-soft-in-out bg-gradient-to-tl from-purple-700 to-pink-500 relative inline-block cursor-pointer whitespace-nowrap border border-solid border-slate-700 text-center align-baseline font-bold uppercase leading-none text-white transition-all duration-200 hover:border-slate-700"
+                            active-color data-color-from="purple-700" data-color-to="pink-500"
+                            onclick="sidebarColor(this)"></span>
+                        <span
+                            class="text-xs rounded-circle h-5.75 mr-1.25 w-5.75 ease-soft-in-out bg-gradient-to-tl from-gray-900 to-slate-800 relative inline-block cursor-pointer whitespace-nowrap border border-solid border-white text-center align-baseline font-bold uppercase leading-none text-white transition-all duration-200 hover:border-slate-700"
+                            data-color-from="gray-900" data-color-to="slate-800" onclick="sidebarColor(this)"></span>
+                        <span
+                            class="text-xs rounded-circle h-5.75 mr-1.25 w-5.75 ease-soft-in-out bg-gradient-to-tl from-blue-600 to-cyan-400 relative inline-block cursor-pointer whitespace-nowrap border border-solid border-white text-center align-baseline font-bold uppercase leading-none text-white transition-all duration-200 hover:border-slate-700"
+                            data-color-from="blue-600" data-color-to="cyan-400" onclick="sidebarColor(this)"></span>
+                        <span
+                            class="text-xs rounded-circle h-5.75 mr-1.25 w-5.75 ease-soft-in-out bg-gradient-to-tl from-green-600 to-lime-400 relative inline-block cursor-pointer whitespace-nowrap border border-solid border-white text-center align-baseline font-bold uppercase leading-none text-white transition-all duration-200 hover:border-slate-700"
+                            data-color-from="green-600" data-color-to="lime-400" onclick="sidebarColor(this)"></span>
+                        <span
+                            class="text-xs rounded-circle h-5.75 mr-1.25 w-5.75 ease-soft-in-out bg-gradient-to-tl from-red-500 to-yellow-400 relative inline-block cursor-pointer whitespace-nowrap border border-solid border-white text-center align-baseline font-bold uppercase leading-none text-white transition-all duration-200 hover:border-slate-700"
+                            data-color-from="red-500" data-color-to="yellow-400" onclick="sidebarColor(this)"></span>
+                        <span
+                            class="text-xs rounded-circle h-5.75 mr-1.25 w-5.75 ease-soft-in-out bg-gradient-to-tl from-red-600 to-rose-400 relative inline-block cursor-pointer whitespace-nowrap border border-solid border-white text-center align-baseline font-bold uppercase leading-none text-white transition-all duration-200 hover:border-slate-700"
+                            data-color-from="red-600" data-color-to="rose-400" onclick="sidebarColor(this)"></span>
+                    </div>
+                </a>
+                <!-- Sidenav Type -->
+                <div class="mt-4">
+                    <h6 class="mb-0">Sidenav Type</h6>
+                    <p class="text-sm leading-normal">Choose between 2 different sidenav types.</p>
+                </div>
+                <div class="flex">
+                    <button transparent-style-btn
+                        class="inline-block w-full px-4 py-3 mb-2 text-xs font-bold text-center text-white uppercase align-middle transition-all border border-transparent border-solid rounded-lg cursor-pointer xl-max:cursor-not-allowed xl-max:opacity-65 xl-max:pointer-events-none xl-max:bg-gradient-to-tl xl-max:from-purple-700 xl-max:to-pink-500 xl-max:text-white xl-max:border-0 hover:scale-102 hover:shadow-soft-xs active:opacity-85 leading-pro ease-soft-in tracking-tight-soft shadow-soft-md bg-150 bg-x-25 bg-gradient-to-tl from-purple-700 to-pink-500 bg-fuchsia-500 hover:border-fuchsia-500"
+                        data-class="bg-transparent" active-style>Transparent</button>
+                    <button white-style-btn
+                        class="inline-block w-full px-4 py-3 mb-2 ml-2 text-xs font-bold text-center uppercase align-middle transition-all bg-transparent border border-solid rounded-lg cursor-pointer xl-max:cursor-not-allowed xl-max:opacity-65 xl-max:pointer-events-none xl-max:bg-gradient-to-tl xl-max:from-purple-700 xl-max:to-pink-500 xl-max:text-white xl-max:border-0 hover:scale-102 hover:shadow-soft-xs active:opacity-85 leading-pro ease-soft-in tracking-tight-soft shadow-soft-md bg-150 bg-x-25 border-fuchsia-500 bg-none text-fuchsia-500 hover:border-fuchsia-500"
+                        data-class="bg-white">White</button>
+                </div>
+                <p class="block mt-2 text-sm leading-normal xl:hidden">You can change the sidenav type just on desktop
+                    view.</p>
+                <!-- Navbar Fixed -->
+                <div class="mt-4">
+                    <h6 class="mb-0">Navbar Fixed</h6>
+                </div>
+                <div class="min-h-6 mb-0.5 block pl-0">
+                    <input navbarFixed
+                        class="rounded-10 duration-250 ease-soft-in-out after:rounded-circle after:shadow-soft-2xl after:duration-250 checked:after:translate-x-5.25 h-5 relative float-left mt-1 ml-auto w-10 cursor-pointer appearance-none border border-solid border-gray-200 bg-slate-800/10 bg-none bg-contain bg-left bg-no-repeat align-top transition-all after:absolute after:top-px after:h-4 after:w-4 after:translate-x-px after:bg-white after:content-[''] checked:border-slate-800/95 checked:bg-slate-800/95 checked:bg-none checked:bg-right"
+                        type="checkbox" />
+                </div>
+            </div>
         </div>
     </div>
-
-    @stack('modals')
-
-    <script src="{{ asset('assets/admin/src/js/index.js') }}"></script>
-    <script src="{{ asset('assets/admin/src/js/us-aea-en.js') }}"></script>
-    <script src="{{ asset('assets/admin/src/js/component/chart-01.js') }}"></script>
-    <script src="{{ asset('assets/admin/src/js/component/chart-02.js') }}"></script>
-    <script src="{{ asset('assets/admin/src/js/component/chart-03.jsa') }}"></script>
-    @livewireScripts
 </body>
+<!-- plugin for charts  -->
+<script src="{{ asset('assets/build/assets/js/plugins/chartjs.min.js') }}" async></script>
+<!-- plugin for scrollbar  -->
+<script src="{{ asset('assets/build/assets/js/plugins/perfect-scrollbar.min.js') }}" async></script>
+<!-- github button -->
+<script async defer src="https://buttons.github.io/buttons.js"></script>
+<!-- main script file  -->
+<script src="{{ asset('assets/build/assets/js/soft-ui-dashboard-tailwind.js') }}" async></script>
 
 </html>
